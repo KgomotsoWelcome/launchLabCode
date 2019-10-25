817cb5efac2b713888a5596e0ab2ff36cd381e9f
KgomotsoWelcome <47862122+KgomotsoWelcome@users.noreply.github.com>
2019-10-25 09:48:41 +0200

    Question 2, completed in 5 minutes

--- Main.java
+++ Main.java
@@ -1,47 +1,26 @@
 /******************************************************************************
-Write a program that prints the numbers from 1 to 100. But for multiples of
-three print 'Fizz' instead of the number and for the multiples of five 
-print 'Buzz'. For numbers which are multiples of both three and five print
-'FizzBuzz'. Start your timer before you attempt this challenge
+Reverse a string. You can not use the python string slicing method which is
+string = string[::-1]
 *******************************************************************************/
+import java.util.*;
+import java.io.*;
 
 public class Main
 {
-    public static boolean Mutliple(int value, int multiple)
+    public static String Reverse(String string)
     {
-        boolean condition = false;
-        if(value%multiple == 0)
+        String reversed = "";
+        for(int i = string.length()-1; i >=0 ; i--)
         {
-            condition = true;
+            reversed = reversed + string.charAt(i);
         }
-        else
-        {
-            condition = false;
-        }
-        
-        return condition;
+        return reversed;
     }
     
 	public static void main(String[] args) {
-		for(int i=1; i<=100;i++)
-		{
-		    if ((Mutliple(i,3) == true) && (Mutliple(i,5) == true))
-		    {
-		        System.out.println("FizzBuzz");
-		    }
-		    else if (Mutliple(i,3) == true)
-		    {
-		        System.out.println("Fizz");
-		    }
-		    else if (Mutliple(i,5) == true)
-		    {
-		        System.out.println("Buzz");
-		    }
-		    else
-		    {
-		        System.out.println(i);
-		    }
-		}
+	    Scanner scan = new Scanner(System.in);
+	    
+		System.out.println(Reverse(scan.nextLine()));
 		
 	}
 }
